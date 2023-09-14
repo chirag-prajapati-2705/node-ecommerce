af1446d8dc6d3303f7e37a8c9268d327111d4f91
chirag-prajapati-2705 <chiragprajapati2705@yahoo.com>
2023-09-14 19:13:07 +0530

    Implementation of theme and functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,8 +4,37 @@ var router = express.Router();
 const Product = mongoose.model("Products");
 
 router.get("/", async (req, res) => {
+    // const limitValue = req.query.limit || 2;
+    // const skipValue = req.query.skip || 0;
+    //
+    // let products = await Product.find().populate("category").limit(limitValue).skip(skipValue);
+    // console.log(products);
+    // res.json({'products':products})
+
+    const page = parseInt(req.query.page || 1);
+    const pageSize = parseInt(req.query.pageSize || 2);
+
+    console.log(page);
+    console.log(pageSize);
+
+    // Calculate the start and end indexes for the requested page
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = page * pageSize;
     let products = await Product.find().populate("category");
-    res.json({'products':products})
+
+    // Slice the products array based on the indexes
+    const paginatedProducts = products.slice(startIndex, endIndex);
+
+    // Calculate the total number of pages
+    const totalPages = Math.ceil(products.length / pageSize);
+
+    // Send the paginated products and total pages as the API response
+
+    const limitValue = req.query.limit || 2;
+    const skipValue = req.query.skip || 0;
+    console.log(products);
+    //res.json({'products':products})
+    res.json({products: paginatedProducts, totalPages })
 });
 
 router.get("/get-product/:_id", async (req, res) => {
@@ -14,15 +43,18 @@ router.get("/get-product/:_id", async (req, res) => {
 });
 
 router.post("/store", async (req, res) => {
-    let {product_name,product_description,product_slug,price,quantity,categories} = req.body;
+    let {product_name,product_description,product_slug,price,quantity,categories,status} = req.body;
     const newProduct = {
         product_name: product_name,
         product_description: product_description,
         product_slug: product_slug,
         price: price,
         quantity: quantity,
-        category:categories
+        category:categories,
+        status:status
     };
+
+    console.log(newProduct);
 
     Product.create(newProduct).then((product) => {
             res.json({'message':'Product has been successfully created!','product':product})
